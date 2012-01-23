59bf7ae3f52ec094b091602e763732b7f6f9681c
c9s <cornelius.howl@gmail.com>
2012-01-23 17:08:28 +0800

    Add new profiles

---
+++ profiling/iterator.php
@@ -0,0 +1,50 @@
+<?php
+require( 'tests/bootstrap.php');
+
+class Internal implements Iterator {
+  protected $a;
+  public function __construct(array $a) {
+    $this->a = $a;
+  }
+  public function current() {
+    return current($this->a);
+  }
+  public function key() {
+    return key($this->a);
+  }
+  public function next() {
+    return next($this->a);
+  }
+  public function rewind() {
+    return reset($this->a);
+  }
+  public function valid() {
+    return (current($this->a) !== FALSE);
+  }
+}
+class External implements IteratorAggregate {
+  protected $a;
+  public function __construct(array $a) {
+    $this->a = $a;
+  }
+  public function getIterator() {
+    return new ArrayIterator($this->a);
+  }
+}
+$a = array('A', 'B', 'C', 'D');
+$internal = new Internal($a);
+$external = new External($a);
+foreach ( $a as $item);
+foreach ($internal as $item);
+foreach ($external as $item);
+
+$bench = new SimpleBench;
+$bench->setN( 50000 );
+$bench->setTitle( 'iterator' );
+
+$bench->iterate( 'func' , 'direct function call' , function() {
+
+});
+
+$result = $bench->compare();
+echo $result->output('console');
---
+++ profiling/json-yaml.php
@@ -0,0 +1,43 @@
+<?php
+require( 'tests/bootstrap.php');
+
+$invoice = array (
+    "invoice"=> 34843,
+    "date"=> 980208000,
+    "product"=> array(
+        array(
+            "sku"=> "BL394D",
+            "quantity"=> 4,
+            "description"=> "Basketball",
+            "price"=> 450,
+          ),
+        array(
+            "sku"=> "BL4438H",
+            "quantity"=> 1,
+            "description"=> "Super Hoop",
+            "price"=> 2392,
+          ),
+      ),
+    "tax"=> 251.42,
+    "total"=> 4443.52,
+    "comments"=> "Late afternoon is best. Backup contact is Nancy Billsmer @ 338-4338.",
+  );
+$foo = array( 'foo' => 1 , 'bar' => array( 'zoo' => 1 ) );
+
+$bench = new SimpleBench;
+$bench->setN( 60000 );
+
+$bench->iterate( 'json_en' , '' , function() use ($invoice) {
+    return json_encode($invoice);
+});
+
+$bench->iterate( 'yaml_emit' , '' , function() use ($invoice) {
+    return yaml_emit($invoice);
+});
+
+$bench->iterate( 'syck_dump' , '' , function() use ($invoice) {
+    return syck_dump($invoice);
+});
+
+$result = $bench->compare();
+echo $result->output('console');
