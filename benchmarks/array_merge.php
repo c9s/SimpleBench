ce55ab0eb4e62b737506767d1bc1f5bcf999eff1
c9s <cornelius.howl@gmail.com>
2012-01-29 15:39:13 +0800

    array_merge

---
+++ benchmarks/array_merge.php
@@ -0,0 +1,20 @@
+<?php
+require( 'tests/bootstrap.php');
+
+$bench = new SimpleBench;
+$bench->setN( 50000 );
+$bench->setTitle( 'array merge' );
+
+$arr1 = array( 'a' => 1 );
+$arr2 = array( 'b' => 2 , 'c' => 3 );
+
+$bench->iterate( 'array_merge' , 'array_merge' , function() use($arr1,$arr2) {
+    $arr = array_merge($arr1, $arr2);
+});
+
+$bench->iterate( '+' , '+' , function() use($arr1,$arr2) {
+    $arr = $arr1 + $arr2;
+});
+
+$result = $bench->compare();
+echo $result->output('console');
