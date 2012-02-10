22c88eccb5aa3cd1a2b0c7645bdea488c499469f
c9s <cornelius.howl@gmail.com>
2012-02-10 17:04:38 +0800

    add count test.

---
+++ benchmarks/count.php
@@ -0,0 +1,45 @@
+<?php
+require( 'tests/bootstrap.php');
+
+$bench = new SimpleBench;
+$bench->setN( 50000 );
+$bench->setTitle( 'Array count' );
+
+class A implements ArrayAccess
+{
+    public $data;
+
+    public function offsetGet($offset) { return $this->data[$offset]; } 
+
+    public function offsetExists($offset) { return isset($this->data[$offset]); } 
+
+    public function offsetUnset($offset) { unset($this->data); } 
+
+    public function offsetSet($offset, $data) { 
+        if (is_array($data)) $data = new self($data); 
+        if ($offset === null) { // don't forget this! 
+            $this->data[] = $data; 
+        } else { 
+            $this->data[$offset] = $data; 
+        } 
+    } 
+}
+
+$a = range(1,10000);
+$bench->iterate( 'count' , 'count()' , function() use ($a) {
+    $b = count($a);
+});
+
+$bench->iterate( 'static count' , 'static count()' , function() use ($a) {
+    static $b;
+    $b = count($a);
+});
+
+
+$aa = new A;
+$bench->iterate( 'array_access' , 'ArrayAccess' , function() use ($aa) {
+    $b = count($aa);
+});
+
+$result = $bench->compare();
+echo $result->output('console');
