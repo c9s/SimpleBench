bf58e6900e828161ea4b02a6c360a7af575cc93d
c9s <cornelius.howl@gmail.com>
2012-04-08 18:27:35 +0800

    ifcondition benchmarks

---
+++ benchmarks/ifconds.php
@@ -0,0 +1,48 @@
+<?php
+require( 'tests/bootstrap.php');
+
+$bench = new SimpleBench;
+$bench->setN( 50000 );
+$bench->setTitle( '' );
+
+function getValue()
+{
+    return true;
+}
+
+$bench->iterate( '=== null' , '=== null' , function() {
+    if( null === getValue() ) {
+    }
+});
+
+$bench->iterate( 'is_null' , 'is_null' , function() {
+    if( is_null(getValue()) ) {
+    }
+});
+
+$bench->iterate( '!' , '!' , function() {
+    if( ! getValue() ) {
+    }
+});
+
+$bench->iterate( '=== false' , 'false' , function() {
+    if( false === getValue() ) {
+
+    }
+});
+
+$bench->iterate( 'boolean context' , 'boolean context' , function() {
+    if ( getValue() ) {
+
+    }
+});
+
+
+$bench->iterate( 'boolean === boolean' , 'boolean context' , function() {
+    if ( true === getValue() ) {
+
+    }
+});
+
+$result = $bench->compare();
+echo $result->output('console');
